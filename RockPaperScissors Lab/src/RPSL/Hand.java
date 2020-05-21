a53817cea43f6410fe156b9c50dcea4c6fb507b7
gp1021 <60611487+gp1021@users.noreply.github.com>
2020-05-21 12:53:42 -0400

    Update Hand.java

--- RockPaperScissors Lab/src/RPSL/Hand.java
+++ RockPaperScissors Lab/src/RPSL/Hand.java
@@ -1,19 +1,24 @@
 package RPSL;
 
 public class Hand {
+	//instance variable
 	private String _move;
 
-    Hand(String move) {
-        _move = move;
-    }
-
-    public String getHandMove() {
-        return _move;
-    }
+	//constructor
+	//contains the move the hand makes, whether it's rock, paper, or scissors
+    	Hand(String move) {
+        	_move = move;
+    	}
+	
+	//getter
+    	public String getHandMove() {
+        	return _move;
+    	}
     
-    public String randomMove() {
-    	String[] x = {"rock", "paper", "scissors"};
-    	int y = (int)(Math.random() * 3);
-    	return x[y];
-    }
+	//picks a random move for the hand
+    	public String randomMove() {
+    		String[] x = {"rock", "paper", "scissors"};
+    		int y = (int)(Math.random() * 3);
+    		return x[y];
+    	}
 }
